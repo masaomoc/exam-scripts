5e750e6561746fc5094499be5f438464e5c09bcd
Masao Mochizuki <masaomoc.0301988@gmail.com>
2013-07-15 22:51:33 +0900

    changed crontab format

--- userdata/ses-publish.sh
+++ userdata/ses-publish.sh
@@ -7,4 +7,6 @@ yum install -y git jq
 git clone https://github.com/masaomoc/exam-scripts.git exam
 chown -R ec2-user:ec2-user exam/
 
-echo '*/1 * * * * /bin/bash /home/ec2-user/exam/publish.sh' | crontab -u ec2-user
+cat << EOC | crontab -u ec2-user -
+*/1 * * * * /bin/bash /home/ec2-user/exam/publish.sh
+EOC
