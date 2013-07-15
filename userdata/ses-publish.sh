2c460ecd8344e8806a47f0787acc8c9e69c3201d
Masao Mochizuki <masaomoc.0301988@gmail.com>
2013-07-15 15:01:21 +0900

    added chown

--- userdata/ses-publish.sh
+++ userdata/ses-publish.sh
@@ -5,5 +5,6 @@ cd /home/ec2-user
 yum install -y git jq
 
 git clone https://github.com/masaomoc/exam-scripts.git exam
+chown -R ec2-user:ec2-user exam/
 
 echo '*/1 * * * * /bin/bash /home/ec2-user/exam/publish.sh' | crontab -u ec2-user
--- userdata/sqs-receive.sh
+++ userdata/sqs-receive.sh
@@ -5,3 +5,4 @@ cd /home/ec2-user
 yum install -y git jq
 
 git clone https://github.com/masaomoc/exam-scripts.git exam
+chown -R ec2-user:ec2-user exam/
